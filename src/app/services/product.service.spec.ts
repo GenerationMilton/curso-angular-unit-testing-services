f9a0785728837502a083ca9ad63c43141ef957d4
Milton Muñoz <livemilton@gmail.com>
2024-07-18 23:33:11 -0500

    generating mocks in unit test with faker-js

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
 import { environment } from "../../environments/environment";
+import { generateManyProducts } from "../models/product.mock";
 import { Product } from "../models/product.model";
 import { ProductsService } from "./product.service";
 
@@ -28,19 +29,7 @@ fdescribe('ProductsService',() =>{
   describe('tests for getAllSimple', ()=>{
     it('should return a product list', (doneFn)=>{
       //Arrange
-      const mockData:Product[] =[
-        {
-          id:'123',
-          title:'title',
-          price:12,
-          description: 'blabla',
-          category:{
-            id:112,
-            name:'as'
-          },
-          images:['img','img']
-        }
-      ];
+      const mockData:Product[] = generateManyProducts(2);
 
       //Act
       productService.getAllSimple()
