d1d70a6c167d7515342db98b64ddf5eece84be2d
Milton Muñoz <livemilton@gmail.com>
2024-07-21 17:08:05 -0500

    config github Actions

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,13 @@ module.exports = function (config) {
       }
     },
     reporters: ['mocha'],
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers:{
+      ChromeHeadlessCI:{
+        base:'ChromeHeadless',
+        flags:['--no-sandbox']
+      }
+    },
     restartOnFileChange: true
   });
 };
